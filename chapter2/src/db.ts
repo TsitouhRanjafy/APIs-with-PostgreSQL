9f782413db63e654aceee08eff1920f3064fbde9
TsitouhRanjafy <radohery09@gmail.com>
2025-03-20 20:36:54 +0100

    definition database

---
+++ chapter2/src/db.ts
@@ -0,0 +1,24 @@
+import { DatabaseSync } from "node:sqlite";
+
+const database = new DatabaseSync(':memory:')
+
+database.exec(`
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`)
+
+database.exec(`
+    CREATE TABLE todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        task TEXT,
+        completed BOOLEAN DAFAULT 0,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+`)
+
+
+
--- chapter2/src/server.ts
+++ chapter2/src/server.ts
@@ -1,22 +1,28 @@
-import dotenv from 'dotenv'
 import express, { Application, urlencoded, Request, Response} from 'express'
-import { StatusCodes } from 'http-status-codes'
 import { createServer } from 'node:http'
+import  path, { dirname }  from "node:path"
+import { fileURLToPath } from 'node:url';
 
-dotenv.config();
+
 const app: Application = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 4000;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename);  
 
+
+// Middleware
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,'../public')))
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(StatusCodes.OK).send(`
-        <h1 style="color:green;font-family: sans-serif;">NODE TS SERVER OK</h1>
-        `)
+
+
+app.get("/",(req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname,'public','index.html'));
 })
 
+
 server.listen(PORT, () => {
-    console.log(` server running at http://localhost:PORT`);
+    console.log(` server running on http://localhost:${PORT}`);
 })
